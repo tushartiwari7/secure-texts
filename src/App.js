b51d4f1bebd46ed1d207ca6aa2444fdf3b4336be
Tushar Tiwari <tushartiwari0172@gmail.com>
2021-05-23 14:56:55 +0530

    Blog init, installation of react-markdown

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
 import './App.css';
+import React,{ useState } from "react";
 import Home from './Home';
 import {	BrowserRouter as Router, Route,Switch } from 'react-router-dom'; 
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from './Components/AppBar';
+import Blog from './Blog';
+function App() {  
+  const [dark, setdark] = useState(false);
+  const [isDecode, setIsDecode] = useState(false);
 
-function App() {
+  const palletType = dark ? "dark" : "light";
+  const darkTheme = createMuiTheme({
+    palette: {
+      type: palletType,
+    }
+  });
+  const handleTheme = () => {
+    setdark(!dark);
+  };
+
+  const handleDecodeEl = (event) => {
+    setIsDecode(event);
+  };
+
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Home />
-        </Route>
-        <Route path="/aboutus" exact>
-          <h2>about page</h2>
-        </Route>
-        <Route path="/blog" exact>
-          <h2>blog page</h2>
-        </Route>
-      </Switch>
+      
+      <ThemeProvider theme={darkTheme} >
+        <CssBaseline />
+        <div className="App">
+          <AppBar checked={dark} handleTheme={handleTheme} isDecode={isDecode} handleDecodeEl={handleDecodeEl} />
+          <Switch>
+            <Route path="/" exact>
+              <Home isDecode={isDecode}/>
+            </Route>
+            <Route path="/aboutus" exact>
+              <h2>about page</h2>
+            </Route>
+            <Route path="/blog" exact>
+              <Blog />
+            </Route>
+          </Switch>
+        </div>
+        </ThemeProvider>
     </Router>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,48 +1,25 @@
-import { Box,Container,CssBaseline } from '@material-ui/core';
+import { Box,Container } from '@material-ui/core';
 import React,{ useState } from "react";
-import './App.css';
-import AppBar from './Components/AppBar';
 import TextEditor from './Components/TextEditor';
 import AvailableCiphers from './Components/AvailableCiphers';
-import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 
-const Home = () => {
-
+const Home = ({isDecode}) => {
+    
     const [rawText,setRawText] = useState('');
-    const [dark, setdark] = useState(false);
-    const [isDecode, setIsDecode] = useState(false);
-  
-    function textHandler(event) {
-      setRawText(event);
-    }
 
-    const palletType = dark ? "dark" : "light";
-  
-    const darkTheme = createMuiTheme({
-      palette: {
-        type: palletType,
+    function textHandler(event) {
+        setRawText(event);
       }
-    });
-    const handleTheme = () => {
-      setdark(!dark);
-    };
-  
-    const handleDecodeEl = (event) => {
-      setIsDecode(event);
-    };
+
     return (
-        <ThemeProvider theme={darkTheme} >
-        <CssBaseline />
-        <div className="App">
-          <AppBar checked={dark} handleTheme={handleTheme} isDecode={isDecode} handleDecodeEl={handleDecodeEl} />
-          <Box m={4} p={4} >
-            <TextEditor textHandler={textHandler}/>
-          </Box>
-            <Container maxWidth="md">
-              <AvailableCiphers rawText={rawText} isDecode={isDecode} />
-            </Container>
-        </div>
-        </ThemeProvider>
+        <>
+        <Box m={4} p={4} >
+        <TextEditor textHandler={textHandler}/>
+        </Box>
+        <Container maxWidth="md">
+            <AvailableCiphers rawText={rawText} isDecode={isDecode} />
+        </Container>
+        </>
     );
 
 }
